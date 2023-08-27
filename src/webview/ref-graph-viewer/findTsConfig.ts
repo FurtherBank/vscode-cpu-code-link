304741662559293d97f6ead47ecd7817a5e28b37
furtherbank <1293874885@qq.com>
2023-08-27 23:22:41 +0800

    refactor: code

--- src/webview/ref-graph-viewer/findTsConfig.ts
+++ src/webview/ref-graph-viewer/findTsConfig.ts
@@ -1,27 +1,24 @@
 import fs from 'fs/promises';
 import path from 'path';
 
-
 export async function findTsConfig(filePath: string): Promise<string> {
-    const currentDir = path.dirname(filePath);
-  
-    try {
-      const tsConfigPath = path.join(currentDir, 'tsconfig.json');
-      console.log('try to access tsConfigPath', tsConfigPath);
-      
-      await fs.access(tsConfigPath);
-      return tsConfigPath;
-    } catch (error) {
-      // If tsconfig.json not found in the current directory, move up one level
-      const parentDir = path.dirname(currentDir);
-  
-      // Check if we've reached the root directory
-      if (parentDir === currentDir) {
-        throw new Error(`tsconfig.json not found from ${filePath}`);
-      }
-  
-      // Recursively search in the parent directory
-      return findTsConfig(parentDir);
+  const currentDir = path.dirname(filePath);
+
+  try {
+    const tsConfigPath = path.join(currentDir, 'tsconfig.json');
+    console.log('try to access tsConfigPath', tsConfigPath);
+    await fs.access(tsConfigPath);
+    return tsConfigPath;
+  } catch (error) {
+    // If tsconfig.json not found in the current directory, move up one level
+    const parentDir = path.dirname(currentDir);
+
+    // Check if we've reached the root directory
+    if (parentDir === currentDir) {
+      throw new Error(`tsconfig.json not found from ${filePath}`);
     }
+
+    // Recursively search in the parent directory
+    return findTsConfig(parentDir);
   }
-  
+}
