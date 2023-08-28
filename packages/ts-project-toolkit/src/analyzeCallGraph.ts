adc7e046e8640d22651b54c4812aee14f88f2efa
furtherbank <1293874885@qq.com>
2023-08-28 19:46:24 +0800

    feat: 防止无限递归

--- packages/ts-project-toolkit/src/analyzeCallGraph.ts
+++ packages/ts-project-toolkit/src/analyzeCallGraph.ts
@@ -4,19 +4,18 @@ import { analyzeExports } from './analyzeExports';
 
 export type CallGraphNode = Omit<ImportInfo, 'namedImports'> & {
   children?: CallGraphNode[];
-}
-
+};
 
 const CodeExtension = ['.ts', '.tsx', '.js', '.jsx'];
 
 /**
- * 通过 realFilePath 判断是否是内部模块。  
- * 以下情况不是内部模块：  
+ * 通过 realFilePath 判断是否是内部模块。
+ * 以下情况不是内部模块：
  * - realFilePath 不存在
  * - realFilePath 包含 node_modules
  * - realFilePath 不是 ts、tsx、js、jsx 文件
- * @param realFilePath 
- * @returns 
+ * @param realFilePath
+ * @returns
  */
 export const isInternalCodeModule = (realFilePath: string | undefined) => {
   if (!realFilePath || realFilePath?.includes('node_modules')) return false;
@@ -25,16 +24,23 @@ export const isInternalCodeModule = (realFilePath: string | undefined) => {
 
 /**
  * 获取一个文件的调用树
- * @param sourceFile 
- * @returns 
+ * @param sourceFile
+ * @returns
  */
-export const analyzeCallGraph = (sourceFile: SourceFile) => {
+export const analyzeCallGraph = (sourceFile: SourceFile, analyzedFiles?: Set<string>) => {
+  if (!analyzedFiles) {
+    analyzedFiles = new Set();
+  }
   const importModulesInfo = analyzeImports(sourceFile);
   return importModulesInfo
     .map((importModuleInfo): CallGraphNode | false => {
       const { realFilePath, importPath, defaultName, namespaceName, namedImports } = importModuleInfo;
+      if (analyzedFiles!.has(realFilePath!)) {
+        return false;
+      }
       if (isInternalCodeModule(realFilePath)) {
         const exportSourceFile = sourceFile.getProject().getSourceFile(realFilePath!);
+        if (realFilePath) analyzedFiles!.add(realFilePath);
         if (exportSourceFile) {
           const { defaultExport, namedExports } = analyzeExports(exportSourceFile!);
           // 这里认为 default 导出的不是 type，只对 named 导出的 type 进行处理
@@ -50,7 +56,7 @@ export const analyzeCallGraph = (sourceFile: SourceFile) => {
               defaultName,
               namespaceName,
               children: analyzeCallGraph(exportSourceFile),
-            }
+            };
           } else {
             return false;
           }
@@ -62,7 +68,7 @@ export const analyzeCallGraph = (sourceFile: SourceFile) => {
         importPath,
         defaultName,
         namespaceName,
-      }
+      };
     })
     .filter(Boolean) as CallGraphNode[];
 };
